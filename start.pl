bfd1422c499876e9dd9a9c0c94a67e098b8bca1f
Steven Wen <73114262+loopfree@users.noreply.github.com>
2021-11-20 19:35:22 +0700

    add syarat win/lose game

--- start.pl
+++ start.pl
@@ -5,7 +5,7 @@ start :-
     write('kepada rentenir sebesar 2.000.000 gold'), nl,
     write('Keesokan harinya, ternyata harga Bitkowin turun'), nl,
     write('secara drastis sehingga Brianaldo menjadi terjerat'), nl,
-    write('utang sebesar 200.00 gold'), nl,
+    write('utang sebesar 200.000 gold'), nl,
     write('Hal ini pun membuat Brianaldo berpikir keras'), nl,
     write('Setelah beberapa jam berpikir keras, Brianaldo'), nl,
     write('tiba-tiba terpikirkan bahwa Ia memiliki ladang di'), nl,
@@ -16,71 +16,75 @@ start :-
     /*Count:  1       2          3           4          5           6           7            8        9      10    11    12 */
     mainLoop(Job,1,0,0,0,0,0,0,0,100,500,1), !.
 
-
+/* Time dan Gold belum di adjust dengan nilai yang seharusnya */
 mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, Expcap, Gold, Time) :-
-    read(Input),
-    (
-        Input == 'status',
-            status(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, Expcap, Gold, Time),
-            mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, Expcap, Gold, Time);
-        Input == 'quest',
-            quest(Lv),
-            mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, Expcap, Gold, Time);
+    (Time < 10) ->
+        (
+            (Gold < 3000) ->
+                read(Input),
+                (
+                    Input == 'status',
+                        status(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, Expcap, Gold, Time),
+                        mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, Expcap, Gold, Time);
+                    Input == 'quest',
+                        quest(Lv),
+                        mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, Expcap, Gold, Time);
 
-        /* Bagian bawah cuman testing add aspek aja */
-        Input == 'addlv',
-            addlv(Lv, LvX),
-            mainLoop(Job, LvX, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, Expcap, Gold, Time);
+                    /* Bagian bawah cuman testing add aspek aja */
+                    Input == 'addlv',
+                        addlv(Lv, LvX),
+                        mainLoop(Job, LvX, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, Expcap, Gold, Time);
 
-        Input == 'addlvfarming',
-            addlvfarmingv(Lvfarming, LvfarmingX),
-            mainLoop(Job, Lv, LvfarmingX, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, Expcap, Gold, Time);
+                    Input == 'addlvfarming',
+                        addlvfarmingv(Lvfarming, LvfarmingX),
+                        mainLoop(Job, Lv, LvfarmingX, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, Expcap, Gold, Time);
 
-        Input == 'addexpfarming',
-            addexpfarmingv(Expfarming, ExpfarmingX),
-            mainLoop(Job, Lv, Lvfarming, ExpfarmingX, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, Expcap, Gold, Time);
+                    Input == 'addexpfarming',
+                        addexpfarmingv(Expfarming, ExpfarmingX),
+                        mainLoop(Job, Lv, Lvfarming, ExpfarmingX, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, Expcap, Gold, Time);
 
-        Input == 'addlvfishing',
-            addlvfishingv(Lvfishing, LvfishingX),
-            mainLoop(Job, Lv, Lvfarming, Expfarming, LvfishingX, Expfishing, Lvranching, Expranching, Expcurr, Expcap, Gold, Time);
+                    Input == 'addlvfishing',
+                        addlvfishingv(Lvfishing, LvfishingX),
+                        mainLoop(Job, Lv, Lvfarming, Expfarming, LvfishingX, Expfishing, Lvranching, Expranching, Expcurr, Expcap, Gold, Time);
 
-        Input == 'addexpfishing',
-            addexpfishingv(Expfishing, ExpfishingX),
-            mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, ExpfishingX, Lvranching, Expranching, Expcurr, Expcap, Gold, Time);
+                    Input == 'addexpfishing',
+                        addexpfishingv(Expfishing, ExpfishingX),
+                        mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, ExpfishingX, Lvranching, Expranching, Expcurr, Expcap, Gold, Time);
 
-        Input == 'addlvranching',
-            addlvranchingv(Lvranching, LvranchingX),
-            mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, LvranchingX, Expranching, Expcurr, Expcap, Gold, Time);
+                    Input == 'addlvranching',
+                        addlvranchingv(Lvranching, LvranchingX),
+                        mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, LvranchingX, Expranching, Expcurr, Expcap, Gold, Time);
 
-        Input == 'addexpranching',
-            addexpranchingv(Expranching, ExpranchingX),
-            mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, ExpranchingX, Expcurr, Expcap, Gold, Time);
+                    Input == 'addexpranching',
+                        addexpranchingv(Expranching, ExpranchingX),
+                        mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, ExpranchingX, Expcurr, Expcap, Gold, Time);
 
-        /* Sistem penambahan exp di bawah sudah menghandle kasus level up */
-        Input == 'addexpcurr',
-            addexpcurrv(Expcurr, ExpcurrX),
-            (
-                (ExpcurrX >= Expcap) ->
-                    write('Level Up!\n'),
-                    addlv(Lv, LvX),
-                    addexpcapv(Expcap, ExpcapX),
-                    mainLoop(Job, LvX, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, ExpcurrX, ExpcapX, Gold, Time)
-                ;mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, ExpcurrX, Expcap, Gold, Time)
-            );
+                    Input == 'addexpcurr',
+                        addexpcurrv(Expcurr, ExpcurrX),
+                        (
+                            (ExpcurrX >= Expcap) ->
+                                write('Level Up!\n'),
+                                addlv(Lv, LvX),
+                                addexpcapv(Expcap, ExpcapX),
+                                mainLoop(Job, LvX, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, ExpcurrX, ExpcapX, Gold, Time)
+                            ;mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, ExpcurrX, Expcap, Gold, Time)
+                        );
 
-        Input == 'addexpcap',
-            addexpcapv(Expcap, ExpcapX),
-            mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, ExpcapX, Gold, Time);
+                    Input == 'addexpcap',
+                        addexpcapv(Expcap, ExpcapX),
+                        mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, ExpcapX, Gold, Time);
 
-        Input == 'addgold',
-            addgoldv(Gold, GoldX),
-            mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, Expcap, GoldX, Time);
+                    Input == 'addgold',
+                        addgoldv(Gold, GoldX),
+                        mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, Expcap, GoldX, Time);
 
-        Input == 'addtime',
-            addtimev(Time, TimeX),
-            mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, Expcap, Gold, TimeX)
-    ).
-
+                    Input == 'addtime',
+                        addtimev(Time, TimeX),
+                        mainLoop(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expranching, Expcurr, Expcap, Gold, TimeX)
+                )
+            ;write('Penagih hutang tiba di kebun anda dan anda langsung melunasi hutang anda \nCONGRATULATION YOU WIN!')
+        )
+    ;write('Penagih hutang tiba di kebun dan mengambil alih kebun \nGAME OVER').
 
 startGame :-
     write('Welcome To Harvest Star'), nl,
@@ -131,4 +135,5 @@ status(Job, Lv, Lvfarming, Expfarming, Lvfishing, Expfishing, Lvranching, Expran
     write('Exp ranching: '), write(Expranching), nl,
     write('Exp: '), write(Expcurr), write('/'), write(Expcap), nl,
     write('Gold: '), write(Gold), nl,
+    write('Time: '), write(Time), nl,
     nl.
