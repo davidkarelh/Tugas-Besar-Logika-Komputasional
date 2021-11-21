ce75e0eec7ff485a2b9bc50d99169e3973794ca3
davidkarelhp <13520154@std.stei.itb.ac.id>
2021-11-21 16:32:48 +0700

    Update fishing dan farming

--- farming.pl
+++ farming.pl
@@ -1,16 +1,26 @@
 :- include('map.pl').
+:- include('Inventory.pl').
 
 :- dynamic((trueMap/1)).
 
-% trueMap([[]])
+% Belum lengkap sepenuhnya
+% Perlu pengimplementasian jenis tanaman dan lambangnya pada map serta implementasinya pada inventori
+% Perlu pengimplementasian waktu untuk implementasi kesiapan panen
+% Exp juga belum diimplementasikan pada file ini
+% Asumsi pemain masih bisa berjalan di atas area yang digali atau ditanam
+
+% trueMap sama seperti map biasa hanya saja simbol tile-nya bisa berubah karena dinamis, di sini digunakan untuk
+% menyimpan simbol tile saat pemain sedang berada di tile tersebut
 
 initiate_true_map :-
     map_init(M),
     scan_player(_, X, Y),
     replace(M, X, Y, '-', MOut),
-    asserta(trueMap(MOut)), !
-    % print_matrix(MOut)
-    .
+    asserta(trueMap(MOut)), !. 
+
+true_map :-
+    trueMap(M),
+    print_matrix(M).
 
 update_true_matrix(New) :-
     trueMap(M),
@@ -26,8 +36,8 @@ validasi_dig(Out) :-
     elmt_in_true_matrix(X, Y, P),
     (
         (P == 'M'; P == 'R'; P == 'H'; p == 'Q'; P == 'o'; P = ('=')) 
-       -> Out is 0, !
-       ; Out is 1, !
+        -> Out is 0, !
+        ; Out is 1, !
     ).
 
 dig :-
@@ -36,35 +46,57 @@ dig :-
     (
         (V =:= 1)
         -> trueMap(M), replace(M, X, Y, ('='), MOut), update_true_matrix(MOut), write('Anda berhasil menggali di tile ini!\n'), !
-        ; write('Anda tidak bisa menggali di tile ini!\n'), !, fail
-    ),
-    trueMap(T),
-    print_matrix(T).
+        ; write('Anda tidak bisa melakukan penggalian di tile ini!\n'), !, fail
+    ). 
     
-% Dari pengecekkan sepertinya ada error pada find_matrix
-cekCur :-
-    scan_player(_, X, Y),
-    write(X), write('\n'), write(Y),
-    map_init(M),
-    find_matrix(M, X, Y + 2, L),
-    write('\n'), write(L). 
-
+    
 validasi_plant(Out) :-
     scan_player(_,X,Y),
     elmt_in_true_matrix(X, Y, P),
     (
-        (P == "=") 
-       -> Out is 1, !
-       ; Out is 0, !
+        (P == ('=')) 
+        -> Out is 1, !
+        ; Out is 0, !
+    ).
+    
+plant :-
+    scan_player(_, X, Y),
+    validasi_plant(V),
+    (
+        (V =:= 1)
+        -> inventory_list(L), totalItem(S, Total),
+            (
+                (Total =:= 0)
+                -> write('Maaf, Inventori Anda kosong, silakan mengisi inventori Anda dengan tanaman panen terlebih dahulu.\n'), !, fail
+                % Disimulasikan jagung memiliki simbol 'c' pada map
+                ; trueMap(M), replace(M, X, Y, ('c'), MOut), update_true_matrix(MOut), write('Anda menanam jagung tile ini!\n'), !
+            )
+        ; write('Anda, tidak bisa menanam pada tile ini, lakukan penggalian terlebih dahulu!'), !, fail
+    ). 
+
+validasi_harvest(Out) :-
+    scan_player(_, X, Y),
+    elmt_in_true_matrix(X, Y, P),
+    (
+        (P == 'c')
+        -> Out is 1, !
+        ; Out is 0, !
     ).
 
-plant :-
-    validasi_plant(V),
-    write(V),
+% Belum diimplementasikan (perlu pengimplementasian waktu terlebih dahulu, disimulasikan jika belum siap panen)
+validasi_kesiapan_panen(Out) :-
+    Out is 0.
+
+harvest :-
     scan_player(_, X, Y),
-    elmt_in_true_matrix(X, Y, L),
-    trueMap(M),
-    print_matrix(M),
-    write(L)
-    
-    .
+    validasi_harvest(V), 
+    (
+        (V =:= 1)
+        -> validasi_kesiapan_panen(VP),
+            (
+                (P =:= 1)
+                -> write('Anda memanen jagung.\n'), !
+                ; write('Tanaman ini belum siap panen, datang lagi jika sudah siap yaa!\n'), !, fail
+            )
+        ; write('Tile ini tidak bisa dipanen.\n'), !, fail
+    ).
---
+++ fishing.pl
@@ -0,0 +1,86 @@
+:- include('map.pl').
+:- include('Inventory.pl').
+
+:- dynamic((rng/1)).
+
+% implementasi randomizer seperti bisa dimodifikasi sesuai exp
+
+rng(50).
+
+generateRandom :-
+    rng(N),
+    retract(rng(N)),
+    get_time(TimeStamp),
+    A is (mod(TimeStamp, 99) + 1),
+    X is mod((A * N + N), 100),
+    asserta(rng(X))
+    . 
+
+
+cek_air_sekeliling(M, X, Y, Out) :-
+    find_matrix(M, X + 1, Y, L),
+    (L == 'o'),
+    Out is 1, !.
+cek_air_sekeliling(M, X, Y, Out) :-
+    find_matrix(M, X, Y + 1, L),
+    (L == 'o'),
+    Out is 1, !.
+cek_air_sekeliling(M, X, Y, Out) :-
+    find_matrix(M, X + 1, Y + 1, L),
+    (L == 'o'),
+    Out is 1, !.
+cek_air_sekeliling(M, X, Y, Out) :-
+    find_matrix(M, X - 1, Y, L),
+    (L == 'o'),
+    Out is 1, !.
+cek_air_sekeliling(M, X, Y, Out) :-
+    find_matrix(M, X, Y - 1, L),
+    (L == 'o'),
+    Out is 1, !.
+cek_air_sekeliling(M, X, Y, Out) :-
+    find_matrix(M, X - 1, Y - 1, L),
+    (L == 'o'),
+    Out is 1, !.
+cek_air_sekeliling(M, X, Y, 0).
+
+validasi_fish(Out) :-
+    scan_player(_, X, Y),
+    map_init(M),
+    cek_air_sekeliling(M, X, Y, V),
+    (
+        (V =:= 1)
+        -> Out is 0, !
+        ; Out is 0, !
+    ).
+
+fish :-
+    validasi_fish(V),
+    (
+        (V =:= 1)
+        -> 
+            generateRandom,
+            rng(N),
+            (
+                (N < 10)
+                -> write('Anda mendapatkan ikan jenis 1')
+            ;   (N < 20)
+                -> write('Anda mendapatkan ikan jenis 2')
+            ;   (N < 30)
+                -> write('Anda mendapatkan ikan jenis 3')
+            ;   (N < 40)
+                -> write('Anda mendapatkan ikan jenis 4')
+            ;   (N < 50)
+                -> write('Anda mendapatkan ikan jenis 5')
+            ;   (N < 60)
+                -> write('Anda mendapatkan ikan jenis 6')
+            ;   (N < 70)
+                -> write('Anda mendapatkan ikan jenis 7')
+            ;   (N < 80)
+                -> write('Anda mendapatkan ikan jenis 8')
+            ;   (N < 90)
+                -> write('Anda mendapatkan ikan jenis 9')
+            ;   (N < 100)
+                -> write('Anda mendapatkan ikan jenis 10')
+            ), !
+        ; write('Anda tidak bisa memancing di tile ini.'), !, fail
+    ).
