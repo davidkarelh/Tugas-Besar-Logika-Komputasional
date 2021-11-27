1374adbc2f09a93f74aa4d20ce97b1ed6988848e
davidkarelhp <13520154@std.stei.itb.ac.id>
2021-11-27 20:00:27 +0700

    Update ranching

--- ranching.pl
+++ ranching.pl
@@ -1,7 +1,4 @@
-:- include('map.pl').
-:- include('Inventory.pl').
-
-:- dynamic((chicken/1, cow/1, sheep/1, pig/1, ostrich/1, tiger/1, mythical_duck/1, trueMap/1)).
+:- dynamic((chicken/1, cow/1, sheep/1, pig/1, ostrich/1, tiger/1, mythical_duck/1, chickenLastTaken/1)).
 
 % Asumsi hewan ternak hanya ada 3 jenis
 % trueMap sama seperti pada farming gunanya
@@ -18,122 +15,172 @@ pig(0).
 ostrich(0).
 tiger(0).
 mythical_duck(0).
-
-initiate_true_map :-
-    map_init(M),
-    scan_player(_, X, Y),
-    replace(M, X, Y, '-', MOut),
-    asserta(trueMap(MOut)), !. 
-
-true_map :-
-    trueMap(M),
-    print_matrix(M).
-
-update_true_matrix(New) :-
-    trueMap(M),
-    retract(trueMap(M)),
-    asserta(trueMap(New)).
+chickenLastTaken(0).
 
 elmt_in_true_matrix(X,Y,L) :-    
     trueMap(M),
     find_matrix(M,X,Y,L).
 
-validasi_ranching(Out) :-
-    scan_player(_, X, Y),
-    elmt_in_true_matrix(X, Y, P),
-    (
-        (P == 'R')
-        -> Out is 1, !
-        ; Out is 0, !
-    ).
 
 showChicken :-
     chicken(Chick),
     Chick > 0,
     write(Chick), write(' chicken\n').
 
-showSheep :-
-    sheep(Sheep),
-    Sheep > 0,
-    write(Sheep), write(' sheep\n').
-
 showCow(Cow) :-
     cow(Cow),
     Cow > 0,
     write(Cow), write(' cow\n').
 
-showRanch :-
-    chicken(Chick),
+showSheep :-
     sheep(Sheep),
+    Sheep > 0,
+    write(Sheep), write(' sheep\n').
+
+showPig :-
+    pig(Pig),
+    Pig > 0,
+    write(Pig), write(' pig\n').
+
+showOstrich :-
+    ostrich(Ostrich),
+    Ostrich > 0,
+    write(Ostrich), write(' ostrich\n').
+
+showTiger :-
+    tiger(Tiger),
+    Tiger > 0,
+    write(Tiger), write(' tiger\n').
+
+showMythicalDuck :-
+    mythical_duck(MythicalDuck),
+    MythicalDuck > 0,
+    write(MythicalDuck), write(' mythical duck\n').
+
+validasi_ranching :-
+    scan_player(_, X, Y),
+    ranch_position(XR, YR),
+    X == XR,
+    Y == YR.
+
+showRanch(Day) :-
+    chicken(Chick),
     cow(Cow),
+    sheep(Sheep),
+    pig(Pig),
+    ostrich(Ostrich),
+    tiger(Tiger),
+    mythical_duck(MythicalDuck),
     (
-        ((Chick + Sheep + Cow) > 0)
-        -> write('\nSelamat datang di Ranch! Kamu memiliki:\n'), showChicken, showSheep, showCow, write('\nApa yang mau kamu lakukan?\n'), !
-        ; write('\nAnda tidak memiliki hewan ternak di Ranch. Silakan coba membeli hewan ternak di Marketplace.\n'), !, fail
+        ((Chick + Cow + Sheep + Pig + Ostrich + Tiger + MythicalDuck) > 0)
+            -> write('\nSelamat datang di Ranch! Kamu memiliki:\n'), showChicken, showCow, showSheep, showPig, showOstrich, showTiger, showMythicalDuck, write('\nApa yang mau kamu lakukan?\n'), chooseRanch(Day), !
+            ; write('\nAnda tidak memiliki hewan ternak di Ranch. Silakan coba membeli hewan ternak di Marketplace.\n'), !, fail
     ).
 
-ranch :-
-    validasi_ranching(V),
+ranch(Day) :-
     (
-        (V =:= 1)
-        -> showRanch, !
+        validasi_ranching
+        -> showRanch(Day), !
         ; write('Tile ini bukan Ranch. Silakan pergi ke Ranch untuk melakukan kegiatan ranch.\n'), !, fail
     ). 
 
-chicken :-
-    validasi_ranching(V),
+chooseRanch(Day) :-
+    read(Animal),
     (
-        (V =:= 1)
-        -> 
+        (
+            Animal == 'chicken',
+                chicken(Chick),
+                (
+                    (Chick > 0)
+                    -> chickenEgg(Day)
+                    ; write('Kamu tidak memiliki chicken pada ranch')
+                ), !
+        );
+        (
+            Animal == 'cow',
+                cow(Cow),
+                (
+                    (Cow > 0)
+                    -> cowMilk
+                    ; write('Kamu tidak memiliki cow pada ranch')
+                ), !
+        );
+        (
+            Animal == 'sheep',
+                sheep(Sheep),
+                (
+                    (Sheep > 0)
+                    -> chickenEgg
+                    ; write('Kamu tidak memiliki sheep pada ranch')
+                ), !
+        );
+        (
+            Animal == 'pig',
+                pig([Pig]),
+                (
+                    (Pig > 0)
+                    -> chickenEgg
+                    ; write('Kamu tidak memiliki pig pada ranch')
+                ), !
+        );
+        (
+            Animal == 'ostrich',
+                ostrich(Ostrich),
+                (
+                    (Ostrich > 0)
+                    -> chickenEgg
+                    ; write('Kamu tidak memiliki ostrich pada ranch')
+                ), !
+        );
+        (
+            Animal == 'tiger',
+                tiger(Tiger),
+                (
+                    (Tiger > 0)
+                    -> chickenEgg
+                    ; write('Kamu tidak memiliki tiger pada ranch')
+                ), !
+        );
+        (
+            Animal == 'mythical_duck',
+                mythical_duck(MythicalDuck),
+                (
+                    (MythicalDuck > 0)
+                    -> chickenEgg
+                    ; write('Kamu tidak memiliki mythical duck pada ranch')
+                ), !
+        )
+    ).
+
+
+chickenEgg(Day) :-
+    chickenLastTaken(D),
+    (
+        (Day > D)
+        ->
             chicken(Chick),
+            generateRandom(N),
             (
-                (Chick > 0)
-                -> 
-                    % Simulasi ayam menghasilkan 2 telur
-                    write('Ayam-ayam Anda menghasilkan 2 telur.\n'),
-                    write('Anda mendapat 2 telur!\n'),
-                    write('Anda mendapat 6 ranching exp!\n'), !
-                ;
-                    write('Tidak ada ayam pada Ranch.\n'), !, fail
-            )
-            
-        ; write('Anda tidak bisa mengambil hasil ternak selain di Ranch.\n'), !, fail
+                (N =< 25)
+                -> write('Ayam kamu belum menghasilkan telur\n'), !
+                ; (N =< 50)
+                -> insertItem(30, Chick * 1), write('Ayam kamu menghasilkan '), write(Chick * 1), write(' telur.\n'), !
+                ; (N =< 75)
+                -> insertItem(30, Chick * 2), write('Ayam kamu menghasilkan '), write(Chick * 1), write(' telur.\n'), !
+                ; insertItem(30, Chick * 3), write('Ayam kamu menghasilkan '), write(Chick * 1), write(' telur.\n'), !
+            ),
+            retract(chickenLastTaken(D)),
+            asserta(chickenLastTaken(Day)), !
+        ; write('Ayam kamu belum menghasilkan telur\n'), !
     ).
 
-sheep :-
-    validasi_ranching(V),
+cowMilk :-
+    cow(Cow),
+    generateRandom(N),
     (
-        (V =:= 1)
-        -> 
-            sheep(Sheep),
-            (
-                (Sheep > 0)
-                -> 
-                    % Simulasi domba belum menghasilkan wool
-                    write('Domba Anda belum menghasilkan wool.\n'),
-                    write('Silakan cek lagi nanti.\n'), !
-                ;
-                    write('Tidak ada domba pada Ranch.\n'), !, fail
-            )
-            
-        ; write('Anda tidak bisa mengambil hasil ternak selain di Ranch.\n'), !, fail
-    ).
-
-cow :-
-    validasi_ranching(V),
-    (
-        (V =:= 1)
-        -> 
-            cow(Cow),
-            (
-                (Cow > 0)
-                -> 
-                    % Simulasi sapi belum menghasilkan susu
-                    write('Sapi Anda belum menghasilkan susu.\n'),
-                    write('Silakan cek lagi nanti.\n'), !
-                ;
-                    write('Tidak ada sapi pada Ranch.\n'), !, fail
-            )
-            
-        ; write('Anda tidak bisa mengambil hasil ternak selain di Ranch.\n'), !, fail
+        (N =< 33)
+        -> write('Ayam kamu belum menghasilkan telur\n'), !
+        ; (N =< 67)
+        -> insertItem(32, Cow * 1), write('Sapi kamu menghasilkan '), write(Chick * 1), write(' botol susu.\n'), !
+        ; insertItem(32, Cow * 2), write('Sapi kamu menghasilkan '), write(Chick * 1), write(' botol susu.\n'), !
     ).
